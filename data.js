7f4cd4021786f220c6c2dd935be37a85d6acf29f
Abdalazim Ahmed <dalodeveloper@gmail.com>
2025-06-18 17:59:30 +0300

    added data file

---
+++ data.js
@@ -0,0 +1,75 @@
+const books = [
+    {
+        title: "Black Swan",
+        author: "684c7fe1005d4a73fce0ea42",
+        description: "About Black Swan",
+        price: 10,
+        cover: "soft cover",
+    },
+    {
+        title: "Skin In The Game",
+        author: "684c7fe1005d4a73fce0ea42",
+        description: "About Skin In The Game",
+        price: 12,
+        cover: "soft cover",
+    },
+    {
+        title: "Fooled By Randomness",
+        author: "684c7fe1005d4a73fce0ea42",
+        description: "About Fooled By Randomness",
+        price: 8,
+        cover: "hard cover",
+    },
+    
+    {
+        title: "The Forty Rules Of Loves",
+        author: "6850880d9a25ee1ff0e81a2b",
+        description: "About The Forty Rules Of Loves",
+        price: 14,
+        cover: "soft cover",
+    },
+    {
+        title: "The Island Of Missing Trees",
+        author: "6850880d9a25ee1ff0e81a2b",
+        description: "About The Island Of Missing Trees",
+        price: 9,
+        cover: "soft cover",
+    },
+    {
+        title: "The Flea Palace",
+        author: "6850880d9a25ee1ff0e81a2b",
+        description: "About The Flea Palace",
+        price: 10,
+        cover: "hard cover",
+    },
+];
+
+const authors = [
+    {
+        firstName: "Robert",
+        lastName: "Kiyosaki",
+        nationality: "USA",
+    },
+    {
+        firstName: "Paulo",
+        lastName: "Coelho",
+        nationality: "Brazil",
+    },
+    {
+        firstName: "Wayne",
+        lastName: "Dyer",
+        nationality: "USA",
+    },
+    {
+        firstName: "Rolf",
+        lastName: "Dobelli",
+        nationality: "Switzerland",
+    },
+];
+
+
+
+module.exports = {
+    books,
+    authors
+};
